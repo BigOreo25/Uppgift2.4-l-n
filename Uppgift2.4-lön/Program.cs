4109eb9f9aa359a6446f1fd4b07537020090d9ec
BigOreo25 <alts07001@utb.vaxjo.se>
2024-10-09 15:04:27 +0200

    Add project files.

---
+++ Uppgift2.4-lön/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace hoppa
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Nu får du chansen att få veta medellönen på dina anställda! Säg Bänkts lön.");
+            int blön = int.Parse(Console.ReadLine());
+            Console.WriteLine("bra, ge mig Svens lön nu");
+            int slön = int.Parse(Console.ReadLine());
+            Console.WriteLine("Finfint. Nu vill jag ha Gunnars (fina) lön");
+            int glön = int.Parse(Console.ReadLine());
+            int summami = (blön + slön + glön) / 3;
+            Console.WriteLine("här är medellönen: " + summami + "kr");
+
+        }
+
+    }
+
+}
